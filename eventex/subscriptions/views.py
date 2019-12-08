6b98083bcdc71e42dfb9cf3eff9d6c8bbdefe7a6
Diego Go <diego.gomes87@gmail.com>
2019-12-08 10:46:10 -0300

    add test and formulario

---
+++ eventex/subscriptions/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+from eventex.subscriptions.form import SubscriptionForm
+
+
+def subscribe(request):
+    context = {'form': SubscriptionForm()}
+    return render(request, 'subscriptions/subscription_form.html', context)
