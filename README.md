c5bb9087070f14a70eafe1638b9504d854f0a597
Diego Go <diego.gomes87@gmail.com>
2019-12-10 14:41:17 -0300

    update test and refactor code in html templates, views and setting

---
+++ README.md
@@ -0,0 +1,15 @@
+# Eventex
+
+System for events, the client ask for something that can introduced the events
+about tecnology.
+
+## How to Development
+
+1 - Git clone this repo<br>
+2 - Building the venv with python 3.5<br>
+3 - Active .venv<br>
+4 - Install requirements.txt<br>
+5 - Run the test -> python manager.py test<br>
+
+For generate sicret key has inside contrib a file python, scret_gen_file<br>
+Just runner in terminal and copy ;)
--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.contrib import messages
 from django.core import mail
 from django.http import HttpResponseRedirect
@@ -9,16 +10,29 @@ from eventex.subscriptions.form import SubscriptionForm
 
 def subscribe(request):
     if request.method == 'POST':
-        form = SubscriptionForm(request.POST)
-        if form.is_valid():
-            msg_email = render_to_string('subscriptions/subscription_email.txt', form.cleaned_data)
-            mail.send_mail('Confirmação de inscrição', msg_email, 'contato@eventex.com.br', ['contato@eventex.com.br',
-                                                                                             form.cleaned_data['email']])
-            messages.success(request, 'Inscrição realizada com sucesso')
-            return HttpResponseRedirect('/inscricao/')
-        else:
-            return render(request, 'subscriptions/subscription_form.html', {'form': form})
+        return create(request)
     else:
-        context = {'form': SubscriptionForm()}
-        return render(request, 'subscriptions/subscription_form.html', context)
+        return new(request)
 
+
+def create(request):
+    form = SubscriptionForm(request.POST)
+    if not form.is_valid():
+        return render(request, 'subscriptions/subscription_form.html', {'form': form})
+
+    # send email
+    _send_mail('Confirmação de inscrição', settings.DEFAULT_FROM_EMAIL, form.cleaned_data['email'], form.cleaned_data,
+               'subscriptions/subscription_email.txt')
+
+    # success email
+    messages.success(request, 'Inscrição realizada com sucesso')
+    return HttpResponseRedirect('/inscricao/')
+
+
+def new(request):
+    return render(request, 'subscriptions/subscription_form.html', {'form': SubscriptionForm()})
+
+
+def _send_mail(subject, from_, to, context, template_name):
+    msg_email = render_to_string(template_name, context)
+    mail.send_mail(subject, msg_email, from_, [from_, to])
